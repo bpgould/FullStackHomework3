4c096d0f0e9a6562245007ad67ab18fe77f835f9
Bennett Gould <bgould@protonmail.com>
2020-09-22 19:31:51 -0400

    added framework for checking user input values against criteria, learned how to get value from modal via DOM

--- script.js
+++ script.js
@@ -14,27 +14,13 @@
 //   passwordText.value = password;
 // }
 // //helper functions
-// function checkInput(input){
-//     if(input<8){
-//         alert("Please input a larger value");
-//         pForSize();
-//     }
-//     if(input>128){
-//         alert("The random generator only supports a maximum password sixze of 128 characters at this time, please enter a smaller value or chain multiple");
-//         pForSize();
-//     }
-// }
+
 // function pForSize(){
 //     var p1 = prompt("Please input the desired length of the password (8-128 characters)");
 //     p1Int = parseInt(p1);
 //     checkInput(p1Int);
 // }
-// function pForCharTypes(){
-//     var p1 = alert("Click 'OK' to select your preferred character types");
-//     if(varp1){
-        
-//     }
-// }
+
 // modal stuff
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -52,7 +38,9 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    checkInput(); //checkInput and handle errors before closing modal
+    
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -60,4 +48,63 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+//check user inputs and respond accordingly 
+function checkInput(){
+    //get values, this allows me to avoid individual listeners for now since I am just checking on close
+    var field1 = document.getElementById("quantity").value;
+    var field2 = document.getElementById("lowercase").value;
+    var field3 = document.getElementById("uppercase").value;
+    var field4 = document.getElementById("numbers").value;
+    var field5 = document.getElementById("specialcharacters").value;
+    //check user entered values
+    checkQuantInput(field1);
+    checkLowerInput(field2);
+    checkUpperInput(field3);
+    checkNumInput(field4);
+    checkSpecialInput(field5); 
+}
+//For now I have added separate function in case I need to modify these later so that they have different requirement
+//This is done largely for program readability/ ease of use
+//This more easily allows for custom output messages -- I could put them in a String array, I may re-work this later
+function checkQuantInput(input){
+    if(input<8){
+        alert("Please input a larger value");
+    }
+    if(input>128){
+        alert("The random generator only supports a maximum password size of 128 characters at this time, please enter a smaller value or chain multiple");
+    }
+}
+function checkLowerInput(){
+    if(input<0){
+        alert("Please input a larger value");
+    }
+    if(input>128){
+        alert("The random generator only supports a maximum password size of 128 characters at this time, please enter a smaller value or chain multiple");
+    }
+}
+function checkUpperInput(){
+    if(input<0){
+        alert("Please input a larger value");
+    }
+    if(input>128){
+        alert("The random generator only supports a maximum password size of 128 characters at this time, please enter a smaller value or chain multiple");
+    }
+}
+function checkNumInput(){
+    if(input<0){
+        alert("Please input a larger value");
+    }
+    if(input>128){
+        alert("The random generator only supports a maximum password size of 128 characters at this time, please enter a smaller value or chain multiple");
+    }
+}
+function checkSpecialInput(){
+    if(input<0){
+        alert("Please input a larger value");
+    }
+    if(input>128){
+        alert("The random generator only supports a maximum password size of 128 characters at this time, please enter a smaller value or chain multiple");
+    }
 }
