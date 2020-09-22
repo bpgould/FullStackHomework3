739bed0e6e955156c92fd83d186ec03698260d70
Bennett Gould <bgould@protonmail.com>
2020-09-22 18:33:27 -0400

    added modal framework

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+// generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
-function writePassword() {
-    var p1 = prompt("Please input the desired length of the password (8-128 characters)");
-    var p1Int = parseInt(p1);
-    checkInput(p1Int);
+// // global variables, needed within scope of multiple functions
+// var p1Int = 0;
+// var charTypes = [];
+// function writePassword() {
+//     pForSize();
+//     console.log(p1Int);
+//     pForCharTypes();
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
+// //helper functions
+// function checkInput(input){
+//     if(input<8){
+//         alert("Please input a larger value");
+//         pForSize();
+//     }
+//     if(input>128){
+//         alert("The random generator only supports a maximum password sixze of 128 characters at this time, please enter a smaller value or chain multiple");
+//         pForSize();
+//     }
+// }
+// function pForSize(){
+//     var p1 = prompt("Please input the desired length of the password (8-128 characters)");
+//     p1Int = parseInt(p1);
+//     checkInput(p1Int);
+// }
+// function pForCharTypes(){
+//     var p1 = alert("Click 'OK' to select your preferred character types");
+//     if(varp1){
+        
+//     }
+// }
+// modal stuff
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  passwordText.value = password;
-
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
-//helper functions
-function checkInput(input){
-    if(input>7&&input<128){
-        return true;
-    }
-    else{
-        return false;
-    }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
