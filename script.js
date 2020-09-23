cd8838cdf09af6d1ba008d39c85b05a549277c93
Bennett Gould <bgould@protonmail.com>
2020-09-22 20:24:52 -0400

    changed from alert to checkValue button

--- script.js
+++ script.js
@@ -23,12 +23,11 @@
 
 // modal
 
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+
+var modal = document.getElementById("myModal");// Get the modal
+var btn = document.getElementById("myBtn");// Get the button that opens the modal
+var checkBtn = document.getElementById("checkBtn");
+var span = document.getElementsByClassName("close")[0];// Get the <span> element that closes the modal
 
 //global variables
 var numQuantInput = 0;
@@ -38,22 +37,22 @@ var numNumsInput = 0;
 var numSpecialInput = 0;
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function(){
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+checkBtn.onclick = function(){
     checkInput(); //checkInput and handle errors before closing modal
-    
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function(){
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function(event){
   if (event.target == modal) {
-    checkInput(); //checkInput and handle errors before closing modal
-
     modal.style.display = "none";
   }
 }
@@ -78,7 +77,10 @@ function checkInput(){
 //This more easily allows for custom output messages -- I could put them in a String array, I may re-work this later
 function checkQuantInput(input){
     if(input<8){
-        alert("Please input a larger value");
+        //alert("Please input a larger value");
+        var error = document.getElementById("error")
+        error.textContent = "Please enter a valid number" 
+        error.style.color = "red" 
     }
     if(input>128){
         alert("The random generator only supports a maximum password size of 128 characters at this time, please enter a smaller value or chain multiple");
